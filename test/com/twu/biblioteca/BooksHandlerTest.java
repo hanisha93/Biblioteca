955bdb399d98c562befc20aed86cff21efc21099
Hanisha <hanishap@thoughtworks>
2015-06-12 01:19:28 +0530

    Added spec to check Calling Print on console to Display List of books

--- test/com/twu/biblioteca/BooksHandlerTest.java
+++ test/com/twu/biblioteca/BooksHandlerTest.java
@@ -1,7 +1,23 @@
 package com.twu.biblioteca;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class BooksHandlerTest {
+
+    @Test
+    public void specToCheckCallingPrintListOfBooksInBibliotecaView() {
+
+        BibliotecaView bibliotecaView = mock(BibliotecaView.class);
+        Books books = mock(Books.class);
+        BooksHandler booksHandler = new BooksHandler(bibliotecaView , books);
+        booksHandler.listBooks();
+
+        verify(bibliotecaView, Mockito.times(1)).printToConsole(books.toString());
+    }
 
 }
