d6e0b8de27ed576093d4deb71e2d2f9084ac68c6
Hanisha <hanishap@thoughtworks>
2015-06-22 12:08:50 +0530

    Added spec to check when login fails

--- test/com/twu/biblioteca/AppTest.java
+++ test/com/twu/biblioteca/AppTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 
 import java.util.HashMap;
 
+import static com.twu.biblioteca.Messages.LOGIN_FAIL;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.*;
 
@@ -33,6 +34,7 @@ public class AppTest {
         when(userValidation.validateUser("001-0001", "hanisha")).thenReturn("librarian");
         when(view.readInput()).thenReturn("3", "001-0001", "hanisha", "1", "7");
         App app = new App();
+
         app.start(view, menuActions, userValidation, menu, menuController, commonMenuAction);
 
         verify(view, times(5)).readInput();
@@ -55,6 +57,7 @@ public class AppTest {
         when(userValidation.validateUser("001-0001", "hanisha")).thenReturn("librarian");
         when(view.readInput()).thenReturn("3", "001-0001", "hanisha", "1", "7");
         App app = new App();
+
         app.start(view, menuActions, userValidation, menu, menuController, commonMenuAction);
 
         verify(view, times(7)).printToConsole(anyString());
@@ -77,9 +80,9 @@ public class AppTest {
         HashMap<String, HashMap<String, MenuAction>> menuActions = new HashMap<>();
         menuActions.put("librarian", menuAction);
         when(userValidation.validateUser("b01-0002", "hanisha")).thenReturn("librarian");
-
         when(view.readInput()).thenReturn("1", "3", "b01-0002", "hanisha", "7");
         App app = new App();
+
         app.start(view, menuActions, userValidation, menu, menuController, commonMenuAction);
 
         verify(userValidation).validateUser("b01-0002", "hanisha");
@@ -100,9 +103,9 @@ public class AppTest {
         HashMap<String, HashMap<String, MenuAction>> menuActions = new HashMap<>();
         menuActions.put("librarian", menuAction);
         when(userValidation.validateUser("b01-0002", "hanisha")).thenReturn("librarian");
-
         when(view.readInput()).thenReturn("1", "3", "b01-0002", "hanisha", "7");
         App app = new App();
+
         app.start(view, menuActions, userValidation, menu, menuController, commonMenuAction);
 
         verify(menuController, times(2)).displayMenu();
@@ -127,10 +130,34 @@ public class AppTest {
         when(userValidation.validateUser("b01-0003", "priya")).thenReturn("member");
         when(view.readInput()).thenReturn("3", "b01-0003", "priya", "0", "3", "b01-0002", "hanisha", "7");
         App app = new App();
+
         app.start(view, menuActions, userValidation, menu, menuController, commonMenuAction);
 
         verify(menuController, times(2)).displayMenu();
     }
 
+    @Test
+    public void specToTestWhenLoginFailedPrintLoginFailMessage() {
+        View view = mock(View.class);
+        UserValidation userValidation = mock(UserValidation.class);
+        HashMap<String, CommonMenuAction> commonMenuAction = new HashMap<>();
+        MenuController menuController = mock(MenuController.class);
+        MenuItemController menuItemController = mock(MenuItemController.class);
+        LibrarianJob librarianJobHandlesBooks = mock(LibrarianJob.class);
+        HashMap<String, MenuAction> menuAction = new HashMap<>();
+        menuAction.put("1", new CheckOutOption(menuItemController, librarianJobHandlesBooks, "book"));
+        HashMap<String, String> menu = new HashMap<>();
+        menu.put("librarian", "menu");
+        HashMap<String, HashMap<String, MenuAction>> menuActions = new HashMap<>();
+        menuActions.put("librarian", menuAction);
+        menuActions.put("member", menuAction);
+        when(userValidation.validateUser("b01-0002", "hanisha")).thenReturn("librarian");
+        when(view.readInput()).thenReturn("3", "b01-0003", "priy", "0", "3", "b01-0002", "hanisha", "7");
+        App app = new App();
+
+        app.start(view, menuActions, userValidation, menu, menuController, commonMenuAction);
+
+        verify(view).printToConsole(LOGIN_FAIL);
+    }
 
 }
